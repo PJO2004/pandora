f723ddef5a89047a1b9b9fd6f15eae34929a0358
PJO2004 <appletreegood123@naver.com>
2023-05-18 17:16:27 +0900

    cmd 수정

--- start.cmd
+++ start.cmd
@@ -1,2 +1,2 @@
 python.exe -m pip install -r ./requirements.txt
-pyinstaller --onefile --noconsole --name FolderHub main.py
+pyinstaller --onefile --noconsole --add-data "folder_icon.png;." --name FolderHub main.py
