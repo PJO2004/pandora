aad188f97da69172355ab8cad9f01cfd0f9546cb
PJO2004 <appletreegood123@naver.com>
2023-05-19 00:12:48 +0900

    기능 업그레이드

--- start.cmd
+++ start.cmd
@@ -1,2 +1,2 @@
 python.exe -m pip install -r ./requirements.txt
-pyinstaller --onefile --noconsole --add-data "folder_icon.png;." --name FolderHub main.py
+pyinstaller --onefile --noconsole --add-data "./data/folder_icon.png;./data/" --name FolderHub main.py
